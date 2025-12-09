91678c2b1c113dbe568413e2a241f3b067d3dde6
Karela King <karelaking277@gmail.com>
2025-12-09 20:52:31 +0530

    add merge sort implementation

---
+++ sorting/merge-sort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int left[n1], right[n2];
+
+    // Copy data into temp arrays
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[l + i];
+
+    for (int j = 0; j < n2; j++)
+        right[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    // Merge the two halves
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
+    }
+
+    // Copy any remaining elements
+    while (i < n1)
+        arr[k++] = left[i++];
+
+    while (j < n2)
+        arr[k++] = right[j++];
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r)
+        return;
+
+    int m = l + (r - l) / 2;
+
+    // Sort first and second halves
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    // Merge the sorted halves
+    merge(arr, l, m, r);
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
